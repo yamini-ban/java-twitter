eb573a170277cf86ba64198a4ba2c194fe858eb2
yamini-ban <yaminibansal7@gmail.com>
2020-03-11 16:33:22 +0530

    configurations accessed through environment variables.

--- src/main/java/com/knoldus/controller/TwitterInstanceImpl.java
+++ src/main/java/com/knoldus/controller/TwitterInstanceImpl.java
@@ -1,25 +1,22 @@
 package com.knoldus.controller;
 
 import com.knoldus.service.TwitterInstance;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterInstanceImpl implements TwitterInstance {
-    private static AccessToken loadAccessToken() {
-        String token = System.getenv("TOKEN_KEY");
-        String tokenSecret = System.getenv("TOKEN_SECRET");
-        return new AccessToken(token, tokenSecret);
-    }
-
     @Override
     public Twitter getTwitterInstance() {
-        String consumerKey = System.getenv("CONSUMER_KEY");
-        String consumerSecret = System.getenv("CONSUMER_SECRET");
-        Twitter instance = new TwitterFactory().getInstance();
-        instance.setOAuthConsumer(consumerKey, consumerSecret);
-        instance.setOAuthAccessToken(loadAccessToken());
-        return instance;
+        Config configFactory = ConfigFactory.load();
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setOAuthConsumerKey(configFactory.getString("consumer.key"))
+                .setOAuthConsumerSecret(configFactory.getString("consumer.secret"))
+                .setOAuthAccessToken(configFactory.getString("token.key"))
+                .setOAuthAccessTokenSecret(configFactory.getString("token.secret"));
+        return new TwitterFactory(configurationBuilder.build()).getInstance();
 
     }
 }
